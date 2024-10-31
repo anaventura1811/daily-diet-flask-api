869f594551917ca668b241fedf4c0da91751383d
Ana Ventura <anaventura1811@gmail.com>
2024-10-31 20:03:25 -0300

    fix: remove comments

--- app.py
+++ app.py
@@ -11,21 +11,6 @@ import os
 
 load_dotenv()
 
-'''
-  Regras da aplicação
-    # 1. Deve ser possível registrar uma refeição feita,
-    # com as seguintes informações:
-    #   - Nome
-    #   - Descrição
-    #   - Data e hora
-    #   - Está dentro ou não da dieta
-    # 2. Deve ser possível editar uma refeição,
-    podendo alterar todos os dados acima
-    3. Deve ser possível apagar uma refeição
-    # 4. Deve ser possível listar todas as refeições de um usuário
-    # 5. Deve ser possível visualizar uma única refeição
-    # 6. As informações devem ser salvas em um banco de dados
-'''
 
 app = Flask(__name__)
 app.register_blueprint(user_routes)
