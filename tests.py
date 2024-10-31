5a2d56b375e1e9b4bc06b3acbfe8b0b0f78c84fa
Ana Ventura <anaventura1811@gmail.com>
2024-10-31 20:50:10 -0300

    feat: add tests to users endpoints

---
+++ tests.py
@@ -0,0 +1,35 @@
+import pytest
+import requests
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+BASE_URL = os.getenv('BASE_URL')
+
+
+def test_create_user():
+    payload = {
+        "username": "teste_de_usuario3",
+        "password": "123senha"
+    }
+    response = requests.post(f"{BASE_URL}/user", json=payload)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+
+
+def test_login_user():
+    payload = {
+        "username": "teste_de_usuario3",
+        "password": "123senha"
+    }
+    response = requests.post(f"{BASE_URL}/login", json=payload)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+
+
+def update_user():
+    pass
