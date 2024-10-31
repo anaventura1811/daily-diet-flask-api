4e4673562fadd8041a15bb55c36c091fdcbf838b
Ana Ventura <anaventura1811@gmail.com>
2024-10-31 16:18:41 -0300

    feat: implement update, read, delete endpoints for user, add logout route

--- app.py
+++ app.py
@@ -70,5 +70,83 @@ def create_user():
         ), 400
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    if username and password:
+        user = User.query.filter_by(username.first())
+        check_password = checkpw(
+            password=str.encode(password),
+            hashed_password=str.encode(user.password))
+        if user and check_password:
+            login_user(user)
+            return jsonify({
+                "message": "Autenticação realizada com sucesso!"
+            })
+    return jsonify({"message": "Credenciais inválidas."}), 400
+
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso"})
+
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+@login_required
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        return jsonify({"username": user.username})
+    return jsonify({"message": "Usuário não encontrado"}), 404
+
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+@login_required
+def update_user(user_id):
+    data = request.json
+    password = data.get("password")
+    user = User.query.get(user_id)
+
+    if user_id != current_user.id and current_user.role != 'admin':
+        return jsonify({"message": "Operação não permitida"}), 403
+
+    if user and password:
+        if len(password) >= 8 and len(password) <= 14:
+            new_password = hashpw(
+                password=str.encode(password),
+                salt=gensalt())
+            user.password = new_password
+            db.session.commit()
+            return jsonify(
+                {"message": f"Usuário {user.username} atualizado com sucesso"}
+            )
+        else:
+            return jsonify(
+                {"message": "A senha deve conter no mínimo 8 caracteres"
+                 + " e no máximo 14"}
+              )
+    else:
+        return jsonify({"message": "Usuário não encontrado"}), 404
+
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+@login_required
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if current_user.role != 'admin':
+        return jsonify({"message": "Operação não permitida"}), 403
+    if user_id == current_user.id:
+        return jsonify({"message": "Deleção não permitida"}), 403
+    if user and user_id != current_user.id:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": f"Usuário {user_id} deletado com sucesso"})
+    return jsonify({"message": "Usuário não encontrado"}), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
